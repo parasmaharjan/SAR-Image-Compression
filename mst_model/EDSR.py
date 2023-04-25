326180df8bbad112aa7aae315aaacdf8bfc0d1f7
parasmaharjan <paras.maharjan@gmail.com>
2023-04-25 13:31:32 -0500

    First commit. Added the models.

---
+++ mst_model/EDSR.py
@@ -0,0 +1,117 @@
+#from . import common 
+from . import MST
+#import MST
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def default_conv(in_channels, out_channels, kernel_size, bias=True, groups=1):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size // 2), bias=bias, groups=groups)
+
+class MSTFusionBlock(nn.Module):
+    def __init__(self, dim_imgfeat, dim_dctfeat, kernel_size=3, conv=default_conv):
+        super(MSTFusionBlock, self).__init__()
+            
+        self.conv_img = nn.Sequential(conv(dim_imgfeat, dim_imgfeat, kernel_size=kernel_size),
+                                      nn.ReLU(True),
+                                      conv(dim_imgfeat, dim_imgfeat, kernel_size=kernel_size))
+        
+        self.conv_dct = nn.Sequential(conv(dim_dctfeat, dim_dctfeat, kernel_size=kernel_size),
+                                      nn.ReLU(True),
+                                      conv(dim_dctfeat, dim_dctfeat, kernel_size=kernel_size))
+        
+        self.stage_tconv = nn.ConvTranspose2d(dim_dctfeat, dim_dctfeat, kernel_size=kernel_size, stride=2, padding=(kernel_size//2))
+        self.msab = MST.MSAB(dim_in=(dim_imgfeat+dim_dctfeat), dim_head=(dim_imgfeat+dim_dctfeat), dim_out=dim_imgfeat, heads=2, num_blocks=1)
+    
+    def forward(self, in_pix, in_dct):    
+        out_pix = self.conv_img(in_pix)
+        out_dct = self.conv_dct(in_dct)
+        out_pix = self.msab(out_pix, self.stage_tconv(out_dct, output_size=in_pix.shape[2:]))
+        #out_pix = self.msab(out_pix, out_dct)
+        return out_pix+in_pix, out_dct+in_dct
+        
+
+class EDSR(nn.Module):
+    def __init__(self, args, conv=default_conv):
+        super(EDSR, self).__init__()
+
+        in_channel1 = args.downsample_1 * args.n_colors #--> (4*2)    #12 --> (4x3)
+        in_channel2 = args.downsample_2 * args.n_colors #48 --> (16x3)
+        out_channel = args.downsample_1 * args.o_colors
+        
+        dim_x1 = args.n_feats_1
+        dim_x2 = args.n_feats_2
+        
+        kernel_size = 3
+        n_basicblock = args.n_resblocks
+        
+        # define head module for pixel input
+        self.head1 = nn.Sequential(nn.Conv2d(in_channels=in_channel1, out_channels=dim_x1//2, kernel_size=kernel_size, padding=(kernel_size//2), stride=1),
+                                nn.PReLU(dim_x1//2),
+                                nn.Conv2d(in_channels=dim_x1//2, out_channels=dim_x1, kernel_size=kernel_size, padding=(kernel_size//2), stride=1)
+                                )
+        
+        self.head2 = nn.Sequential(nn.Conv2d(in_channels=in_channel2, out_channels=dim_x2//2, kernel_size=kernel_size, padding=(kernel_size//2), stride=1),
+                               nn.PReLU(dim_x2//2),
+                               nn.Conv2d(in_channels=dim_x2//2, out_channels=dim_x2, kernel_size=kernel_size, padding=(kernel_size//2), stride=1)
+                               )
+        
+        self.body = nn.ModuleList([ MSTFusionBlock(dim_x1, dim_x2, kernel_size) for _ in range(n_basicblock) ])
+        
+        
+        # define tail module
+        self.tail = conv(dim_x1, out_channel, kernel_size)
+        
+      
+    def forward(self, x):
+        x1 = F.pixel_unshuffle(x, 2)
+        x2 = F.pixel_unshuffle(x, 4)
+        
+        x1 = self.head1(x1)
+        x2 = self.head2(x2)
+        
+        for i, layer in enumerate(self.body):
+            if i == 0:
+                res_x1, res_x2 = layer(x1, x2)
+            else:
+                res_x1, res_x2 = layer(res_x1, res_x2)
+            
+        res_x1 += x1
+        res_x1 = self.tail(res_x1)
+        #print(res_x1.shape)
+        res_x1 = F.pixel_shuffle(res_x1, 2)
+        res_x1 += x
+        return res_x1
+
+    def load_state_dict(self, state_dict, strict=True):
+        own_state = self.state_dict()
+        for name, param in state_dict.items():
+            if name in own_state:
+                if isinstance(param, nn.Parameter):
+                    param = param.data
+                try:
+                    own_state[name].copy_(param)
+                except Exception:
+                    if name.find('tail') == -1:
+                        raise RuntimeError('While copying the parameter named {}, '
+                                           'whose dimensions in the model are {} and '
+                                           'whose dimensions in the checkpoint are {}.'
+                                           .format(name, own_state[name].size(), param.size()))
+            elif strict:
+                if name.find('tail') == -1:
+                    raise KeyError('unexpected key "{}" in state_dict'
+                                   .format(name))
+
+#------------------------------------------------------------------------------
+
+if __name__=="__main__":
+    # import numpy as np
+    # from .. import option
+    # model = EDSR(args).cuda()
+    # img = torch.tensor(torch.ones(32,2,128,128)).cuda()
+    # print(img.shape)
+    # out = model(img)
+    # print(out.shape)
+    print("Done...")
